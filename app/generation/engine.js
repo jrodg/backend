f77c50b65513860e6988d86edae2bccc8e0128b7
juniorrodriguez <jrod.201@hotmail.com>
2022-04-29 16:48:18 -0400

    added db

--- app/generation/engine.js
+++ app/generation/engine.js
@@ -1,28 +1,38 @@
-const Generation = require('.')
+const Generation = require('./index');
+const GenerationTable = require('./table');
 
-class GenerationEngine{
-    constructor(){
-        this.generation = null; 
-        this.timer = null;
-    }
+class GenerationEngine {
+  constructor() {
+    this.generation = null;
+    this.timer = null;
+  }
 
-    start() {
-        this.buildNewGeneration();
-    }
+  start() {
+    this.buildNewGeneration();
+  }
 
-    stop() {
-        clearTimeout(this.timer)
-    }
+  stop() {
+    clearTimeout(this.timer);
+  }
 
-    buildNewGeneration(){
-        this.generation = new Generation();
+  buildNewGeneration() {
+    const generation = new Generation();
 
-        console.log('new generation', this.generation)
+    GenerationTable.storeGeneration(generation)
+      .then(({ generationId }) => {
+        this.generation = generation;
 
-       this.timer = setTimeout(() => this.buildNewGeneration(),
-            this.generation.expiration.getTime() - Date.now()
-        )
-    };
+        this.generation.generationId = generationId;
+
+        console.log('new generation', this.generation);
+
+        this.timer = setTimeout(
+          () => this.buildNewGeneration(),
+          this.generation.expiration.getTime() - Date.now()
+        );
+      })
+      .catch(error => console.error(error));
+  }
 }
 
 module.exports = GenerationEngine;
